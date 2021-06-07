5702be22e3e21d259f541d4ceb9848ce38daf284
thiago miziara <thiagonmiziara@gmail.com>
2021-06-07 16:06:46 -0300

    apresentando valores do inputs e limpando os dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,18 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
-  transferir(){
-     console.log('solicitado nova transferência')
-     console.log('valor', this.valor)
-     console.log('destino', this.valor)
+  transferir() {
+    console.log('solicitado nova transferência');
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
