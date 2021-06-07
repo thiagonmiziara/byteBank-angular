e76dba5e928bf6e0d64dfe30e0d5e57ef7b937d2
thiago miziara <thiagonmiziara@gmail.com>
2021-06-07 11:52:45 -0300

    add formsmodule do angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
@@ -10,7 +11,8 @@ import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferen
     NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
