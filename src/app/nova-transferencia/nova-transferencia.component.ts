9dfb0456ecfd7ecb112ed49fe9d46e6b6c5f7663
thiago miziara <thiagonmiziara@gmail.com>
2021-06-08 16:41:12 -0300

    projeto finalizado

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Transferencia } from 'src/models/transferencia.model';
 import { TransferenciaService } from '../services/transferencia.service';
 
@@ -13,7 +14,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService) {}
+  constructor(private service: TransferenciaService, private router: Router) {}
 
   transferir() {
     console.log('solicitado nova transferência');
@@ -25,6 +26,9 @@ export class NovaTransferenciaComponent {
     this.service.adicionar(valorEmitir).subscribe((resultado) => {
       console.log(resultado);
       this.limparCampos();
+
+      this.router.navigateByUrl('extrato')
+
     }, error => console.error(error));
   }
 
