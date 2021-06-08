ddc40dc6f2ab3e0e9402b871fa4bf2c9aa5361c1
thiago miziara <thiagonmiziara@gmail.com>
2021-06-08 11:13:20 -0300

    add service e separando as responsabilidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencias: any[] = [];
+
+  constructor(private service: TransferenciaService) {}
 
   transferir($event) {
     console.log($event);
-
-    const tranferencia = {...$event, data: new Date(),}
-    this.transferencias.push(tranferencia);
+    this.service.adicionar($event);
   }
 }
